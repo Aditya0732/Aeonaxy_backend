9ca65cab53fa178d3946d4c438cd73f13dfa58c9
Aditya <patiladitya7219@gmail.com>
2024-04-11 01:08:51 +0530

    bug fixed

--- utils/sendVerificationMail.js
+++ utils/sendVerificationMail.js
@@ -1,10 +1,22 @@
 const {createMailTransporter} = require("./createMailTransporter");
 
-const sendVerificationMail = (user) => {
+const sendVerificationMail = async (user) => {
     const transporter = createMailTransporter();
 
     // Construct the verification link based on your application's route
     const verificationLink = `https://aeonaxy-frontend.onrender.com/verify?emailToken=${user.emailToken}`;
+
+    await new Promise((resolve, reject) => {
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
 
     const mailOptions = {
         from: '"Aditya Aeonaxy Assignment" <aeonaxyaditya@outlook.com>',
@@ -18,13 +30,19 @@ const sendVerificationMail = (user) => {
         `,
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Verification email sent successfully");
-        }
+    await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
     });
+    
+    res.status(200).json({ status: "OK" });
 };
 
 module.exports = { sendVerificationMail };
